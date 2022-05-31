c54a893c17305dca64f66cb56f8cef33a8674182
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-31 16:35:18 +0530

    [Madhavi] create simple linked list

---
+++ LinkedList/LinkedList/LinkedListDS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedListDS
+    {
+
+        public Node head;
+        public void Create(int value)
+        {
+            Node node1 = new Node(value);
+            if (this.head == null)
+            {
+                this.head = node1;
+            }
+            else
+            {
+                Node position = head;
+                while (position.next != null)
+                {
+                    position = position.next;
+                }
+                position.next = node1;
+            }
+            Console.WriteLine("{0} inserted into LinkedList", node1.data);
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1.Create LinkedList \n2.Add new node in LinkedList \n3.Append value in LinkedList" +
+                "\n4.Insert Value in LinkedList");
+            Console.WriteLine("Enter a your option:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            LinkedListDS linkList = new LinkedListDS();
+            switch (option)
+            {
+                case 1:
+                        linkList.Create(56);
+                        linkList.Create(30);
+                        linkList.Create(70);
+                        //list.Display();
+                        break;
+                case 2:
+                    
+                        break;
+                    
+                case 3:
+                   
+                        break;
+                    
+                case 4:
+                        break;
+                    
+                default:
+                        break;
+                    
+            }
+        }
+    }
+}
